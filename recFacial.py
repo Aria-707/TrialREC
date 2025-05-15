44b796269ac537edc5efcb70325c595dafe3580e
Cristian Rodriguez <calejandrorodriguezr@ucundinamarca.edu.co>
2025-05-14 22:26:20 -0500

    Codigo Funcional

--- recFacial.py
+++ recFacial.py
@@ -9,7 +9,24 @@ import requests
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate('asistenciaconreconocimiento-firebase-adminsdk-fbsvc-793e372c66.json')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": "googleapis.com",
+})
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
